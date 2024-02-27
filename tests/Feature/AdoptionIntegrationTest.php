a7da0fa59a3730a594028123196081957d8ec5e4
Caroline Sampaio <caroline_08022@hotmail.com>
2024-02-26 22:55:01 -0300

    feat: adiciona teste integracao listagem adocoes - ex05

--- tests/Feature/AdoptionIntegrationTest.php
+++ tests/Feature/AdoptionIntegrationTest.php
@@ -2,16 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\Adoption;
 use App\Models\Pet;
 use App\Models\Race;
 use App\Models\Specie;
+use App\Models\User;
 use Tests\TestCase;
 
 class AdoptionIntegrationTest extends TestCase
 {
     public function test_adoption_integration(): void
     {
-
+        //cadastro com dados inválidos
         $specie = Specie::factory()->create();
         $race = Race::factory()->create();
         $pet  = Pet::factory()->create(['race_id' => $race->id, 'specie_id' => $specie->id]);
@@ -35,6 +37,7 @@ class AdoptionIntegrationTest extends TestCase
             'data' => []
         ]);
 
+        //cadastro com dados válidos
         $body = [
             'name' => 'Caroline',
             'contact' => '8599181-1333',
@@ -50,6 +53,46 @@ class AdoptionIntegrationTest extends TestCase
         $response->assertStatus(201)->assertJson([
             ...$body,
             'status' => 'PENDENTE',
+        ]);
+
+        //listagem de todas as adoções
+        Adoption::factory(4)->create(['pet_id' => $pet->id]);
+
+        $user = User::factory()->create(['profile_id' => 2, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->get('/api/adoptions');
+
+        $response->assertStatus(200)->assertJsonCount(5)->assertJsonStructure([
+            '*' => [
+                'id',
+                'name',
+                'contact',
+                'email',
+                'cpf',
+                'observations',
+                'pet_id',
+                'status',
+                'created_at',
+                'updated_at',
+            ]
+        ]);
+
+        //listagem de adoção com filtro
+        $response = $this->actingAs($user)->get('/api/adoptions?search=Caroline');
+
+        $response->assertStatus(200)->assertJsonCount(1)->assertJsonStructure([
+            '*' => [
+                'id',
+                'name',
+                'contact',
+                'email',
+                'cpf',
+                'observations',
+                'pet_id',
+                'status',
+                'created_at',
+                'updated_at',
+            ]
         ]);
     }
 }
