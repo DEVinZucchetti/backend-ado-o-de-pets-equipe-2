6bf1995988e73e2e0438e907ad999bce060894f3
Drew <84882983+vdr3w@users.noreply.github.com>
2024-03-04 16:04:48 -0300

    feat(Ex02 - Testes unitários - Rota de listagens de adoções)

--- tests/AdoptionTest.php
+++ tests/AdoptionTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Models\Adoption;
 use App\Models\Pet;
 use App\Models\Race;
 use App\Models\Specie;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -66,6 +68,32 @@ class AdoptionTest extends TestCase
             'status' => 400,
             'errors' => [],
             'data' => []
+        ]);
+    }
+
+    public function test_can_get_all_adoptions(): void
+    {
+        $specie = Specie::factory()->create();
+        $race = Race::factory()->create();
+        $pet  = Pet::factory()->create(['race_id' => $race->id, 'specie_id' => $specie->id]);
+        Adoption::factory(10)->create(['pet_id' => $pet->id]);
+
+        $user = User::factory()->create(['profile_id' => 3, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->get('/api/adoptions');
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            '*' => [
+                'id' => true,
+                'name' => true,
+                'email' => true,
+                'cpf' => true,
+                'contact' => true,
+                'observations' => true,
+                'status' => true,
+                'pet_id' => true
+            ]
         ]);
     }
 }
