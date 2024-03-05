5275db24bd94621004426acf320f574ec194868f
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-05 14:55:48 -0300

    Feat adiciona teste de listagem e aprovação de adoção

--- tests/Unit/AdoptionTest.php
+++ tests/Unit/AdoptionTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Models\Adoption;
+use App\Models\People;
 use App\Models\Pet;
 use App\Models\Race;
 use App\Models\Specie;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -51,4 +54,62 @@ class AdoptionTest extends TestCase
            'data'=>[]
         ]);
     }
+
+  /*  public function test_can_get_all_adoptions(): void
+    {
+        $specie = Specie::factory()->create();
+        $race = Race::factory()->create();
+        $pet  = Pet::factory()->create(['race_id' => $race->id, 'specie_id' => $specie->id]);
+        Adoption::factory(10)->create(['pet_id' => $pet->id]);
+
+        $user = User::factory()->create(['profile_id' => 3, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->get('/api/adoptions');
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            '*' => [
+                'id' => true,
+                'name' => true,
+                'email' => true,
+                'cpf' => true,
+                'contact' => true,
+                'observations' => true,
+                'status' => true,
+                'pet_id' => true
+            ]
+        ]);
+    }*/
+    public function test_user_can_add_realized_adoption(): void
+    {
+        $specie = Specie::factory()->create();
+        $race = Race::factory()->create();
+        $pet  = Pet::factory()->create(['race_id' => $race->id, 'specie_id' => $specie->id]);
+        $adoption = Adoption::factory()->create(['pet_id' => $pet->id]);
+
+        $user = User::factory()->create(['profile_id' => 3, 'password' => '12345678']);
+        $this->assertDatabaseHas('adoptions',['id'=>$adoption->id,'status'=>'PENDENTE']);
+
+        $response = $this->actingAs($user)->post('/api/adoptions/realized',['adoption_id'=>$adoption->id]);
+
+        $this->assertDatabaseHas('adoptions',['id'=>$adoption->id,'status'=>'APROVADO']);
+        $this->assertDatabaseHas('peoples', ['email' => $adoption->email, 'cpf' => $adoption->cpf]);
+        $this->assertDatabaseCount('clients',1);
+
+        $people = People::query()->where(['cpf' => $adoption->cpf])->first();
+        $this->assertDatabaseHas('clients', ['people_id' => $people->id]);
+        $people->load('client');
+        $this->assertDatabaseHas('pets', ['id' => $pet->id, 'client_id' => $people->client->id]);
+      //  $this->assertDatabaseHas('solicitations_documents', ['client_id' =>  $people->client->id]);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'id' => true,
+            'people_id' => true,
+            'bonus' => true
+        ]);
+    }
+
+
 }
+
